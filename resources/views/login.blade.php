b3232fefec9828bd1899d854ff5d0ea310c26954
BimaharDaud <bimahardaud@student.telkomuniversity.ac.id>
2023-11-23 11:19:02 +0700

    tes

---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login Page</title>
+  <link rel="stylesheet" href="login.css">
+
+</head>
+<body>
+<div id="bg"></div>
+
+<div class="containerLogin">
+  <img src="logo.jpg" alt="logo">
+  <form>
+    <div class="form-field">
+      <input type="username" placeholder="Username" required/>
+    </div>
+    
+    <div class="form-field">
+      <input type="password" placeholder="Password" required/>                     
+  </div>
+    
+    <div class="form-field">
+      <button class="btn" type="submit">LOGIN</button>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
