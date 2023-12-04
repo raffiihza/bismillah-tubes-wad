43ee10b45aa8edf4338b4db873d2f4be35d95fa4
hilmiikhsan <robbinhood22012@gmail.com>
2023-12-04 21:55:20 +0700

    fix responsive

--- resources/views/pages/beranda-pasien.blade.php
+++ resources/views/pages/beranda-pasien.blade.php
@@ -9,10 +9,10 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <nav class="flex justify-between fixed w-full z-[10] items-center px-10 py-4 bg-[#D9D9D9] flex-col md:flex row">
+    <nav class="flex justify-between fixed w-full z-[10] items-center px-10 py-4 bg-[#D9D9D9]">
         <div class="flex gap-4 items-center">
             <img src="/telkomedika-logo.png" alt="logo" class="w-[70px]">
-            <div>
+            <div class="hidden md:block">
                 <ul class="flex gap-2">
                     <li>
                         <a href="">Antrian</a>
@@ -29,27 +29,27 @@
         </div>
     </nav>
 
-    <main class="relative min-h-screen overflow-hidden pt-[10rem]">
+    <main class="relative min-h-screen overflow-hidden pt-[10rem] pb-[5rem]">
         <img src="/bg.jpeg" alt="bg" class="absolute contrast-50 h-full top-0 w-full bg-cover">
 
         <div class="relative z-[9]">
             <div class="flex flex-col gap-5 items-center">
                 <h1 class="text-5xl font-bold  text-white">13.13</h1>
                 <h2 class="uppercase text-red-600 font-bold text-4xl">Selamat datang</h2>
-                <h3 class="bg-red-600 inline-block px-3 text-sm font-bold py-1 rounded-md text-white uppercase">Di website Telkomedika</h3>
+                <h3 class="bg-red-600 inline-block px-7 text-sm font-bold py-3 rounded-full text-white uppercase">Di website Telkomedika</h3>
             </div>
 
-            <div class="mt-8 max-w-2xl mx-auto flex flex-col md:flex-row gap-10 justify-between items-center">
-                <div class="bg-white rounded-md w-[250px]">
+            <div class="mt-[8rem] md:mt-8 max-w-2xl mx-auto flex flex-col md:flex-row gap-10 justify-between items-center">
+                <div class="bg-white rounded-md w-full max-w-[300px]">
                     <img class="w-[150px] block mx-auto" src="/people.png" alt="people">
-                    <div class="mt-2 border-t flex justify-center py-2">
-                        <a href="" class="px-4 py-2 bg-red-600 font-bold text-white text-sm rounded-full ">Daftar Antri</a>
+                    <div class="mt-2 border-t flex justify-center py-2 px-5 md:px-0 text-center">
+                        <a href="{{ route('form-pendaftaran') }}" class="px-4 py-2 bg-red-600 block w-full md:w-auto md:inline-block font-bold text-white text-sm rounded-full ">Daftar Antri</a>
                     </div>
                 </div>
-                <div class="bg-white rounded-md w-[250px]">
+                <div class="bg-white rounded-md w-full max-w-[300px]">
                     <img class="w-[100px] block mx-auto" src="/document.png" alt="document">
-                    <div class="mt-2 border-t flex justify-center py-2">
-                        <a href="" class="px-4 py-2 bg-red-600 font-bold text-white text-sm rounded-full ">Informasi</a>
+                    <div class="mt-2 border-t flex justify-center py-2 px-5 md:px-0 text-center">
+                        <a href="{{ route('artikel') }}" class="px-4 py-2 bg-red-600 block w-full md:w-auto md:inline-block font-bold text-white text-sm rounded-full ">Informasi</a>
                     </div>
                 </div>
             </div>
--- resources/views/pages/form-pendaftaran.blade.php
+++ resources/views/pages/form-pendaftaran.blade.php
@@ -1,17 +1,21 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Form Pendaftaran</title>
+    <title>Form pendaftaran</title>
+
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
-<body class="bg-gray-100">
-    <div class="h-screen relative flex items-center w-full mx-auto">
-        <img src="/bg.jpeg" class="absolute object-cover h-full brightness-[2] contrast-[75%] top-0 w-full z-[-10]" alt="">
-        <div class="absolute w-full h-full z-[-9] bg-white/60"></div>
+<body>
+    <div class=" h-screen relative flex items-center w-full mx-auto">
+        <img src="/bg.jpeg" class="absolute object-cover  h-full brightness-[2] contrast-[75%] top-0 w-full z-[-10]" alt="">
+        <div class="absolute w-full h-full z-[-9] bg-white/60">
+
+        </div>
 
         <div class="flex flex-col w-full items-center">
             <img src="/telkomedika-logo.png" alt="">
@@ -19,7 +23,7 @@
             <h1 class="my-5 text-xl font-bold">Form Pendaftaran</h1>
 
             <form>
-                <div class="bg-white p-3 rounded-xl grid gap-4">
+                <div class="bg-white p-3 rounded-xl grid gap-6">
                     <div class="flex">
                         <label for="" class="font-bold w-[125px]">Tipe Pasien</label>
                         <div>
